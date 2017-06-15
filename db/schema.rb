19d201948e8273306702073a7fb09d9be166185d
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-06-15 13:31:45 +0900

    Create users_events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615041649) do
+ActiveRecord::Schema.define(version: 20170615042957) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -74,6 +74,15 @@ ActiveRecord::Schema.define(version: 20170615041649) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "users_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "event_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_users_events_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_users_events_on_user_id", using: :btree
+  end
+
   create_table "users_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                    null: false
     t.integer  "group_id",                   null: false
@@ -89,6 +98,8 @@ ActiveRecord::Schema.define(version: 20170615041649) do
   add_foreign_key "groups", "events"
   add_foreign_key "groups", "restaurants"
   add_foreign_key "restaurants", "locations"
+  add_foreign_key "users_events", "events"
+  add_foreign_key "users_events", "users"
   add_foreign_key "users_groups", "groups"
   add_foreign_key "users_groups", "users"
 end

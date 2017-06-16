7fe23230be78f312058ea089a9e64a59c01bab4e
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-06-16 11:12:07 +0900

    Add name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615133750) do
+ActiveRecord::Schema.define(version: 20170616021127) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170615133750) do
     t.string   "job"
     t.string   "university"
     t.string   "image"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end

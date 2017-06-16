0549cf5354ac4f0f13ee4047bed1b64d0b3dd385
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-06-17 00:01:18 +0900

    Add saving event_user action

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,7 +1,11 @@
 class EventUsersController < ApplicationController
   def create
-    event = Event.find(params[:event_id])
-    # ToDo 処理を書く
-    redirect_to event_path(event)
+    @event = Event.find(params[:event_id])
+    event_user = EventUser.new({event_id: @event.id, user_id: current_user.id})
+    if event_user.save
+      redirect_to event_path(@event)
+    else
+      render template: "events/show"
+    end
   end
 end

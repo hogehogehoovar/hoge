1522a554b795a0be203be6655e70484ea2a0c6e3
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-07-02 16:20:27 +0900

    Update ruby and python coopration

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -3,8 +3,18 @@ class EventUsersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # ToDo Pythonに処理を飛ばすと、勝手にevent_user, group, group_userをつくってくれる。
     event = Event.find(params[:event_id])
+
+    # ToDo Pythonに処理を飛ばすと、勝手にevent_user, group, group_userをつくってくれる。
+    RubyPython.start(:python_exe => "python2.7")
+    dir = Rails.root.join('sandbox').to_s
+    sys = RubyPython.import 'sys'
+    sys.path.append File.join(dir)
+    called_ruby = RubyPython.import("called_ruby")
+    called_ruby.print_python
+    called_ruby.print_python_with_argument!( arg1: event.id, arg2: current_user.id )
+    RubyPython.stop
+
     event_user = EventUser.find_or_create_by( { event_id: event.id, user_id: current_user.id } )
     group = Group.find_or_initialize_by( { event_id: event.id, restaurant_id: event.facility.restaurants.first.id } )
     GroupUser.find_or_create_by( { group_id: group.id, user_id: current_user.id } )
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,28 +12,4 @@ class GroupsController < ApplicationController
       marker.infowindow location.name
     end
   end
-
-  # def create
-  #   event = Event.find(params[:event_id])
-  #   group = Group.new
-  #   group.event = event
-  #   group.users = event.users
-  #   group.restaurant = Restaurant.first # ToDo ここの処理を書く
-
-  #   # Python連携のサンプルコード
-  #   RubyPython.start(:python_exe => "python2.7")
-  #   dir = Rails.root.join('sandbox').to_s
-  #   sys = RubyPython.import 'sys'
-  #   sys.path.append File.join(dir)
-  #   called_ruby = RubyPython.import("called_ruby")
-  #   called_ruby.print_python
-  #   called_ruby.print_python_with_argument!( arg1: event.id )
-  #   RubyPython.stop
-
-  #   if group.save
-  #     redirect_to event_group_path(event, group)
-  #   else
-  #     redirect_to event_path(event)
-  #   end
-  # end
 end

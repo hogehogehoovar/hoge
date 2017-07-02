f55e3ab30c3c731f6a7ce4c58df908d422661a3e
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-07-02 17:35:38 +0900

    Add attending group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,11 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @users = @group.users
+    @group_user = GroupUser.find_by(group_id: @group.id, user_id: current_user.id)
     @event = Event.find(params[:event_id])
     @restaurant = @group.restaurant
     @facility = @event.facility
     locations = [@restaurant, @facility]
-
     @hash = Gmaps4rails.build_markers(locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude

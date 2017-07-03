a033444404f26b3f38c5458f309c445cbf7d651e
sonodaatom <sonodaatom@MacBook-Pro-114.local>
2017-07-03 22:56:17 +0900

    mysqlの情報を使いやすいようにとってきた

--- python/clustering.py
+++ python/clustering.py
@@ -6,30 +6,24 @@ from sklearn.cluster import KMeans
 import pymysql.cursors
 import sys
 from functools import reduce
+from datetime import datetime
 
 #引数
 argvs = sys.argv
 eventId=argvs[1]
 userId=argvs[2]
 
-#参加人数
-participants=0
-#グループ数
-group_num=0
-#グループ
-group=[]
-#グループごとの人数
-group_participants=[]
-#1人のみのグループ数
-group1_num=0
-#2人のみのグループ数
-group2_num=0
+#イベントに参加しているユーザー
+#userList=[]
+#グループとユーザの対応
+group_users={}
+#ユーザの情報
+userDic={}
+
 #新しいグループを量産するフェーズかどうか
 isCreateFase=False
 #新しいグループを量産するフェーズの人数の閾値
 PARTICIPANTNUM=15
-#コサイン類似度のリスト
-cosList=[ ]
 #最大のコサイン類似度
 maxcos=0
 
@@ -43,56 +37,70 @@ def sql():
     cursor = connection.cursor()
 
     # Select結果を取り出す
-    sql = 'select gender,birthday,job,university from users where id=%s'
-    cursor.execute(sql,userId)
+    sql_user = 'select  user_id from event_users where event_id=%s'
+    cursor.execute(sql_user,eventId)
     results = cursor.fetchall()
-    print(results)
-    sql_num = 'select count( user_id) from event_users where event_id=%s'
-    cursor.execute(sql_num,eventId)
+    global userList
+    userList = [x[0] for x in results]
+    print('user')
+    print(userList)
+
+    sql_userInfo = 'select gender,birthday,job,university from users where id=%s'
+    global userDic
+    userDic = {user: getCursor(cursor,sql_userInfo,user) for user in userList}
+    print(userDic)
+
+    sql_group_user = 'select group_id,user_id from group_users '
+    cursor.execute(sql_group_user)
     results = cursor.fetchall()
-    global participants
-    participants = int(results[0][0])
-    sql_groupnum = 'select count( event_id) from groups where event_id=%s'
-    cursor.execute(sql_groupnum,eventId)
-    results = cursor.fetchall()
-    global group_num
-    group_num = int(results[0][0])
-    print("group_num")
-    print(type(group_num))
-    print(group_num)
-    sql_group = 'select user_id,group_id from group_users '
-    cursor.execute(sql_group)
-    results = cursor.fetchall()
-    global group
-    group=results
+    global group_users
+
+    group_users={k: {} for k, v in results}
+    print( "@@@")
+    {k: group_users[k].update({v:userDic[v]}) for k, v in results}
+    #group_users={k: [] for k, v in results}
+    #{k: group_users[k].append(v) for k, v in results}
+    print(group_users)
+
+
     #cursor切断
     cursor.close
 
-def getGroup():
-    global group_participants
-    global group1_num
-    global group2_num
-
-    for i in range(group_num):
-        group_participants.append(len([x for x in group if x[1] ==i+1]))
-        if group_participants[i]==1:
-            group1_num+=1
-        if group_participants[i]==2:
-            group2_num+=1
+def getCursor(cursor,sql,key):
+    cursor.execute(sql,key)
+    results = cursor.fetchall()
+    return results[0]
 
 def getFase():
     global isCreateFase
-    if participants<PARTICIPANTNUM:
+    if len(userList)<PARTICIPANTNUM:
         isCreateFase=True
 
+def getGroup():
+    group1=[k for k,v in group_users.items() if len(v)==1]
+    print(group1)
+    #未完成のグループ(8人未満)
+    group = {k:v for k,v in group_users.items() if len(v)<8 and len(v)>0}
+    print( group)
+
+    if not len(group1)==0:
+        insert(group1)
+    else:
+        #cos_simの計算
+        cosDic={k:[]  for k, v in group.items()}
+        {k: group[k].append(v) for k, v in group.items()}
+        #if isCreateFase:
 
 
 
-    '''
-    # K-means クラスタリングをおこなう
-    kmeans_model = KMeans(n_clusters=3, random_state=10).fit_predict(results)
-    print( kmeans_model)
-    '''
+def insert(groupId):
+    with pymysql.connect(user='sonoda', password='sonoda', host='localhost', database='webEng') as cur:
+        now = datetime.today()
+        str_now = now.isoformat()
+        cur.execute("INSERT INTO group_users (user_id,group_id,attendance,created_at,updated_at) VALUES (%s,%s,%s,%s,%s)", (int(userId),int(groupId),0,str_now,str_now))
+
+
+
 def closeSql():
     connection = pymysql.connect(user='sonoda', password='sonoda', host='localhost', database='webEng')
     #mysql切断
@@ -105,6 +113,7 @@ if __name__ == '__main__':
     x = np.array([1, 1, 1, 1, 1])
     y = np.array([1, 0, 1, 0, 1])
     z = np.array([0, 1, 0, 0, 0])
+
     sql()
 
     getGroup()

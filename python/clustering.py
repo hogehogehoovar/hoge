89e2e2ade95c84933026e8ef16e1752bcf7fd0b2
sonodaatom <sonodaatom@MacBook-Pro-114.local>
2017-07-04 12:36:41 +0900

    とりあえず、1人以上7人以下の未完成グループ（未G）数を取得し、 未G＝０ならG新規作成して投入 未G＞０かつ1人の未完成グループ（１G）あるなら、そこに投入 未G＞０かつ1人の未完成グループ（１G）ないなら、cos類似度に基づき、G選んで投入orG新規作成して投入

--- python/clustering.py
+++ python/clustering.py
@@ -15,18 +15,17 @@ userId=argvs[2]
 
 #イベントに参加しているユーザー
 #userList=[]
+#参加中のユーザの情報辞書
+userDic={}
 #グループとユーザの対応
 group_users={}
-#ユーザの情報
-userDic={}
 
 #新しいグループを量産するフェーズかどうか
 isCreateFase=False
 #新しいグループを量産するフェーズの人数の閾値
 PARTICIPANTNUM=15
-#最大のコサイン類似度
-maxcos=0
-
+#現在時刻
+now = datetime.today()
 
 
 def sql():
@@ -36,32 +35,27 @@ def sql():
     # カーソル取得
     cursor = connection.cursor()
 
-    # Select結果を取り出す
+    # イベントに参加しているユーザーリスト
     sql_user = 'select  user_id from event_users where event_id=%s'
     cursor.execute(sql_user,eventId)
     results = cursor.fetchall()
     global userList
     userList = [x[0] for x in results]
-    print('user')
-    print(userList)
 
+    #参加中のユーザの情報辞書
     sql_userInfo = 'select gender,birthday,job,university from users where id=%s'
     global userDic
     userDic = {user: getCursor(cursor,sql_userInfo,user) for user in userList}
-    print(userDic)
+    #userDicをcos_sim計算しやすいように正規化
+    userDic={k:(v[0],(now.year-v[1].year)/80,ord(v[2])/65535,ord(v[3])/65535) for k,v in userDic.items()}
 
+    #グループとユーザの対応
     sql_group_user = 'select group_id,user_id from group_users '
     cursor.execute(sql_group_user)
     results = cursor.fetchall()
     global group_users
-
     group_users={k: {} for k, v in results}
-    print( "@@@")
     {k: group_users[k].update({v:userDic[v]}) for k, v in results}
-    #group_users={k: [] for k, v in results}
-    #{k: group_users[k].append(v) for k, v in results}
-    print(group_users)
-
 
     #cursor切断
     cursor.close
@@ -71,6 +65,7 @@ def getCursor(cursor,sql,key):
     results = cursor.fetchall()
     return results[0]
 
+#新しいグループ作るかどうか->参加人数が閾値以下だと作成
 def getFase():
     global isCreateFase
     if len(userList)<PARTICIPANTNUM:
@@ -78,27 +73,46 @@ def getFase():
 
 def getGroup():
     group1=[k for k,v in group_users.items() if len(v)==1]
-    print(group1)
+    group2=[k for k,v in group_users.items() if len(v)==2]
     #未完成のグループ(8人未満)
     group = {k:v for k,v in group_users.items() if len(v)<8 and len(v)>0}
-    print( group)
 
-    if not len(group1)==0:
+    #未完成のグループ０個なら新しくグループ作る
+    if len(group)==0:
+        createGroup()
+    #1人のグループあるならとりあえずそこに入れる
+    elif not len(group1)==0:
         insert(group1)
+    #それ以外
     else:
-        #cos_simの計算
-        cosDic={k:[]  for k, v in group.items()}
-        {k: group[k].append(v) for k, v in group.items()}
-        #if isCreateFase:
-
+        #cos類似度計算
+        cosDic={k:{ }  for k, v in group.items()}
+        {k: cosDic[k].update({k2:cos_sim(userDic[int(userId)],v2)}) for k, v in group.items() for k2,v2 in v.items()}
+        cosAve={k:np.average(list(v.values())) for k,v in cosDic.items() }
+        #cos類似度の大きいグループに入れる
+        maxCos=max([(v,k) for k,v in cosAve.items()])[0]
+        maxGroup=max([(v,k) for k,v in cosAve.items()])[1]
+        if isCreateFase:
+            #cos類似度が十分に大きくなければ新しいグループ作るー＞グループ多いほど、作られにくい、2人のグループあるなら作られにくい
+            if maxCos <0.8/(len(group)+len(group2)):
+                createGroup()
+            else:
+                insert(maxGroup)
+        else:
+            if maxCos <0.5/(len(group)+len(group2)):
+                createGroup()
+            else:
+                insert(maxGroup)
 
 
 def insert(groupId):
     with pymysql.connect(user='sonoda', password='sonoda', host='localhost', database='webEng') as cur:
-        now = datetime.today()
         str_now = now.isoformat()
         cur.execute("INSERT INTO group_users (user_id,group_id,attendance,created_at,updated_at) VALUES (%s,%s,%s,%s,%s)", (int(userId),int(groupId),0,str_now,str_now))
 
+def createGroup():
+    maxGroupId=max([k for k,v in group_users.items()])
+    insert(maxGroupId+1)
 
 
 def closeSql():
@@ -110,18 +124,9 @@ def cos_sim(v1, v2):
     return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 if __name__ == '__main__':
-    x = np.array([1, 1, 1, 1, 1])
-    y = np.array([1, 0, 1, 0, 1])
-    z = np.array([0, 1, 0, 0, 0])
-
     sql()
-
-    getGroup()
     getFase()
-
+    getGroup()
     closeSql()
     
-    print(cos_sim(x, y))
-    print(cos_sim(y, z))
-    print(cos_sim(z, x))
 

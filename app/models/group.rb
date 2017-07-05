04fd7bb7daa799894168f6aa042a9a6b234ec444
阿久澤圭 <akusawakei@MacBook-Pro-9.local>
2017-07-05 19:55:24 +0900

    Add baseline

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   belongs_to :event
   belongs_to :restaurant
+  scope :search_by_user_and_event, -> (user_id, event_id) { joins(:group_users).find_by("group_users.user_id = ? AND event_id = ?", user_id, event_id) }
 end

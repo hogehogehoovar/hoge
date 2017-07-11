6bbb38784a7667b0f3f277ce0671de4fb398cb70
阿久澤圭 <akusawakei@MacBook-Pro-10.local>
2017-07-11 19:42:20 +0900

    Comment about current_event

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -5,6 +5,7 @@ class Facility < ApplicationRecord
   has_many :restaurants
 
   def current_event
+    # OR じゃなくて AND だけど、デモとして動く状態を保つ為このままにする
     events.find_by("(start_time <= ?) OR (end_time >= ?)", DateTime.now, DateTime.now)
   end
 end

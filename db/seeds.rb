4578d433c2b0e555b1788dfb07a404629d180aa4
阿久澤圭 <akusawakei@MacBook-Pro-106.local>
2017-07-18 20:03:58 +0900

    Edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,24 @@ def get_alphabet(n)
 end
 
 50.times do |num|
-  User.create!(email: "test#{num}@gmail.com", password: '000000', name: Faker::Pokemon.name, job: get_alphabet(Faker::Number.between(1, 10)), university: get_alphabet(Faker::Number.between(1, 10)), gender: Faker::Number.between(0, 1), birthday: Faker::Date.birthday(18, 35), image: 'https://www.pakutaso.com/shared/img/thumb/TRT0607001_TP_V.jpg' )
+  User.create(email: "test#{num}@gmail.com", password: '000000', name: Faker::Pokemon.name, job: get_alphabet(Faker::Number.between(1, 10)), university: get_alphabet(Faker::Number.between(1, 10)), gender: Faker::Number.between(0, 1), birthday: Faker::Date.birthday(18, 35), image: 'https://www.pakutaso.com/shared/img/thumb/TRT0607001_TP_V.jpg' )
+end
+
+person_arr = ['https://www.pakutaso.com/shared/img/thumb/00_PP45_PP_TP_V.jpg',
+              'https://www.pakutaso.com/shared/img/thumb/TRT0607001_TP_V.jpg',
+              'https://www.pakutaso.com/shared/img/thumb/TRTM9355_TP_V.jpg',
+              'https://www.pakutaso.com/shared/img/thumb/0I9A5450ISUMI_TP_V.jpg',
+              'https://www.pakutaso.com/shared/img/thumb/IMARI20160807103519_TP_V.jpg',
+              'https://www.pakutaso.com/shared/img/thumb/TSURU170321-85%20mm-087_TP_V.jpg',
+              ]
+
+50.times do |num|
+  User.create(email: "web#{num}@gmail.com",
+              password: '000000',
+              name: Gimei.last.kanji,
+              job: Faker::Job.field,
+              university: Faker::University.name,
+              gender: Faker::Number.between(0, 1),
+              birthday: Faker::Date.birthday(18, 35),
+              image: person_arr.shuffle.sample )
 end
